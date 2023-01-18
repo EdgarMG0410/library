efcf49a0fd881ef0f66119ec7e482fdaf38a5de0
EdgarMG <emancillas04@gmail.com>
2023-01-18 00:24:39 -0600

    Migrate to next13

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import styles from "src/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.Home}>
+        <div className={styles.title}>
+          <h1>The Book Store</h1>
+          <p>
+            Est duis adipisicing exercitation elit magna ex tempor reprehenderit. Non cillum dolore occaecat pariatur adipisicing occaecat proident do reprehenderit do. Sit ex ipsum eu eiusmod nostrud irure cupidatat Lorem mollit esse tempor. Deserunt laborum deserunt pariatur est deserunt dolore aliqua pariatur consectetur commodo do minim ipsum. Quis consectetur occaecat cupidatat fugiat minim pariatur aute irure sint non sunt aute. Quis adipisicing cupidatat deserunt aliqua anim cupidatat.
+          </p>
+        </div>
+        <div className={styles.side}></div>
+      </main>
+    </>
+  )
+}
